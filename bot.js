2a9749035833d45f664bcb3a0e3575a54adb829f
Renaldy Pratama <renaldy.pratama@bizzy.co.id>
2020-07-14 19:44:46 +0700

    Integrate with chatbot backend

--- bot.js
+++ bot.js
@@ -1,5 +1,21 @@
-var botBuilder = require('claudia-bot-builder')
+const botBuilder = require('claudia-bot-builder')
+const { searchWikipedia } = require('./src/api/wikipedia')
+require('dotenv').config()
 
-module.exports = botBuilder(function (request) {
-  return 'Hi this is a test'
-});
+function FactBot (request) {
+  const query = request.text
+  return searchWikipedia(query)
+    .then(res => {
+      const answer = res.data.data
+      if (!answer.length) {
+        return 'Sorry, the item you searched is not found.'
+      }
+      return answer
+    })
+    .catch(error => {
+      console.log(error)
+      return 'Sorry, I encountered an error.'
+    })
+}
+
+module.exports = botBuilder(FactBot);
